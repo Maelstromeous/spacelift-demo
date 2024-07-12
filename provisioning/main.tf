c3d7a7b07a10eb689b99655a15f433bf7326f212
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-07-12 14:36:09 +0100

    Reverted to hardcoding directories

--- provisioning/main.tf
+++ provisioning/main.tf
@@ -1,7 +1,7 @@
 # Local file resource to create the zip file
 resource "null_resource" "zip_files" {
   provisioner "local-exec" {
-    command = "cd ${var.source_directory} && zip -r ${path.module}/${var.zip_file} src"
+    command = "cd .. && zip -r provisioning/app.zip src"
   }
 
   triggers = {
@@ -15,17 +15,12 @@ resource "aws_s3_bucket" "app_bucket" {
   tags   = var.tags
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.app_bucket.id
-  acl    = "private"
-}
-
 # S3 bucket object
 resource "aws_s3_object" "upload_zip" {
   depends_on = [null_resource.zip_files]
 
   bucket = aws_s3_bucket.app_bucket.bucket
   key    = var.zip_file
-  source = "${path.module}/${var.zip_file}"
+  source = "app.zip"
   server_side_encryption = "AES256"
 }
